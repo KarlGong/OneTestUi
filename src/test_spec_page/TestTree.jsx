2b2200c6d5c63e84ca4ff3d0f1c0c202544eafdc
Karl Gong <karl.gong@outlook.com>
2017-07-05 11:50:34 +0800

    Add spinner when loading tree.

--- src/test_spec_page/TestTree.jsx
+++ src/test_spec_page/TestTree.jsx
@@ -1,17 +1,20 @@
 import React, {Component} from "react";
 import {observer} from "mobx-react";
 import {observable, action} from "mobx";
-import {Layout, Menu, Icon, Tree} from "antd";
+import {Layout, Menu, Icon, Tree, Spin} from "antd";
 import axios from "axios";
 import event from "event";
 
 @observer
 export default class TestTree extends Component {
     @observable treeData = [];
+    @observable loading = false;
 
     componentWillMount() {
         self = this;
+        this.loading = true;
         axios.get("/api/project/1/rootsuite").then(function (response) {
+            self.loading = false;
             self.treeData.push(response.data);
         })
     }
@@ -31,18 +34,22 @@ export default class TestTree extends Component {
         });
 
         return (
-            <Tree
-                class="draggable-tree"
-                draggable
-                showLine
-                showicon
-                loadData={this.loadChildren.bind(this)}
-                onDragEnter={this.onDragEnter}
-                onDrop={this.onDrop}
-                onSelect={this.handleSelect.bind(this)}
-            >
-                {loop(this.treeData)}
-            </Tree>
+            <Spin spinning={this.loading}>
+                <div style={{height: "100%"}}>
+                <Tree
+                    class="draggable-tree"
+                    draggable
+                    showLine
+                    showicon
+                    loadData={this.loadChildren.bind(this)}
+                    onDragEnter={this.onDragEnter}
+                    onDrop={this.onDrop}
+                    onSelect={this.handleSelect.bind(this)}
+                >
+                    {loop(this.treeData)}
+                </Tree>
+                </div>
+            </Spin>
         );
     }
 
