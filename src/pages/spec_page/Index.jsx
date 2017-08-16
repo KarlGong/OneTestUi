e6066d8db7b18cc46e455ec023c6c2bd1f8fc0f4
Karl Gong <karl.gong@outlook.com>
2017-08-16 14:01:38 +0800

    Implement resize panel.

--- src/pages/spec_page/Index.jsx
+++ src/pages/spec_page/Index.jsx
@@ -3,18 +3,23 @@ import {observable} from "mobx";
 import {Layout, Menu, Icon, Tree} from "antd";
 import TabbedPanel from "~/components/TabbedPanel";
 import TestSpecTree from "./TestSpecTree";
+import SplitPane from "react-split-pane";
 
 class SpecPage extends Component {
 
     render() {
         return (
             <Layout>
-                <Layout.Sider width={200} style={{background: "#fff", borderRight: "1px #c0bdbd solid"}}>
-                    <TestSpecTree/>
-                </Layout.Sider>
-                <Layout style={{ background: "#fff", width: "calc(100% - 200px)"}}>
-                    <TabbedPanel/>
-                </Layout>
+                <SplitPane split="vertical" minSize={100} maxSize={1000}
+                           defaultSize={parseInt(localStorage.getItem("oneTest-TestSpec-LeftPane-Width")) || 250}
+                           onChange={size => localStorage.setItem("oneTest-TestSpec-LeftPane-Width", size)}>
+                    <Layout style={{background: "#fff"}}>
+                        <TestSpecTree/>
+                    </Layout>
+                    <Layout style={{background: "#fff"}}>
+                        <TabbedPanel/>
+                    </Layout>
+                </SplitPane>
             </Layout>
         );
     }
